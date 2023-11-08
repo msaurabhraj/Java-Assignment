24d53dc42089c53c835367148a9290acc3852139
msaurabhraj <116434792+msaurabhraj@users.noreply.github.com>
2023-11-08 11:26:26 +0530

    changed the program

--- unit_2/interface.java
+++ unit_2/interface.java
@@ -1,6 +1,8 @@
 interface il {
     int a = 10 ;
+    //int a; throws an error , variable must be assigned
     public void disp();
+    // public void disp1(); every method shoud be override 
 
 }
 class C1 implements il{
@@ -10,10 +12,16 @@ class C1 implements il{
     }
 
 }
-
+class C2 implements il {
+    public void disp(){
+        System.out.println("class C2");
+    }
+}
 class InterFaceDemo{
     public static void main(String[] args) {
         C1 c = new C1();
+        c.disp();
+         C2 c2 = new C2();
         c.disp();
         
     }
